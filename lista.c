27b98cf9a5e82bb63296dd640f9e0993e61063f6
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-01-17 21:04:20 +0100

    first commit

---
+++ lista.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node
+{
+    int val;
+    struct Node *next;
+};
+
+struct Node *listAdd(struct Node *list, int val);
+void listPrint(struct Node *pn);
+int listLenght(struct Node *list);
+int listPosition(struct Node *list, int val);
+void listRemoveAt(struct Node *list, int val);
+
+int main(){
+    struct Node *list = NULL;
+    char input[25] = "";
+    while (strcmp(input, "stop") != 0)
+    {
+        printf("Inserisci un numero per inserirlo nella lista, o \"stop\" per terminare: ");
+        scanf("%s", &input);
+
+        if (strcmp(input, "stop") != 0)
+        {
+            list = listAdd(list, atoi(input));
+        }
+    }
+    printf("Lungezza: %d\n", listLenght(list));
+    int pos;
+    printf("Inserisci un numero da cercare nella lista: ");
+    scanf("%d", &pos);
+    pos = listPosition(list, pos);
+    if (pos != -1)
+    {
+        printf("valore trovato alla posizione %d\n", pos + 1);
+    } else
+    {
+        printf("valore non presente nella lista\n");
+    }
+    printf("Lista:\n");
+    listPrint(list);
+    int rem;
+    printf("Inserisci la posizione da rimuovere: ");
+    scanf("%d", &rem);
+    listRemoveAt(list, rem - 1);
+    printf("Lista con il valore rimosso:\n");
+    listPrint(list);
+}
+
+struct Node *listAdd(struct Node *list, int val) {
+    struct Node *p = list;
+    // If the list is not empty the pointer goes on until the last one
+    // Else allo
+    if (list != NULL)
+    {
+        while (p -> next != NULL)
+        {
+            p = p -> next;
+        }
+        
+        p -> next = malloc(sizeof(struct Node));
+        p = p -> next;
+    } else {
+        list = malloc(sizeof(struct Node));
+        p = list;
+    }
+    p -> val = val;
+    p -> next = NULL;
+    return list;
+}
+
+int listLenght(struct Node *list) {
+    int l = 0;
+    if (list != NULL)
+    {
+        l++;
+        while (list -> next != NULL)
+        {
+            l++;
+            list = list -> next;
+        }
+    }
+    return l;
+}
+
+int listPosition(struct Node *list, int val) {
+    int p = 0;
+    if (list != NULL)
+    {
+        while (list -> next != NULL && list -> val != val)
+        {
+            p++;
+            list = list -> next;
+        }
+        if (list -> next == NULL && list -> val != val)
+        {
+            p = -1;
+        }
+    } else {
+        p = -1;
+    }
+    return p;
+}
+
+void listRemoveAt(struct Node *list, int val) {
+    val--;
+    if (list != NULL)
+    {
+        for (int i = 0; i < val && list != NULL; i++)
+        {
+            list = list -> next;
+        }
+        if (list != NULL)
+        {
+            struct Node *p = list -> next;
+            if (p != NULL)
+            {
+                list -> next = p -> next;
+                free(p);
+            } else {
+                list -> next = p;
+            }
+        }
+    }
+}
+
+void listPrint(struct Node *pn) {
+    while (pn){
+        printf("%d ",pn->val);
+        pn = pn -> next;
+    }
+    printf("\n");
+}
